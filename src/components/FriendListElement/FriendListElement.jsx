97db8348da0fd9cb1d55f2479213fea07e1bc1b3
calmighted <manishh@tutanota.com>
2022-01-10 11:12:20 +0530

    added height to friendList container

--- src/components/FriendListElement/FriendListElement.jsx
+++ src/components/FriendListElement/FriendListElement.jsx
@@ -5,16 +5,15 @@ import Style from "./FriendListElement.module.css"
 
 export default function FriendListElement({user}) {
     return (
-			<li className={Style.rightbarFriend}>
-				<div className={Style.rightbarProfileImgContainer}>
+			<li className={Style.Friend}>
+				<div className={Style.ProfileImgContainer}>
 					<img
-						className={Style.rightbarProfileImg}
+						className={Style.ProfileImg}
 						src={user.profilePicture}
 						alt=""
 					/>
-					{/* <span className={Style.rightbarOnline}></span> */}
 				</div>
-				<span className={Style.rightbarUsername}>{user.username}</span>
+				<span className={Style.Username}>{user.username}</span>
 			</li>
 		);
 }
